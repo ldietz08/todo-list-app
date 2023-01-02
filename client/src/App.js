80eda1314b91c5f7911199b9bfe37e872a7def09
Lakpa Dietz <ldietz086@gmail.com>
2023-01-01 19:15:14 -0800

    Added post functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,82 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
 import axios from "axios";
-import addIcon from "./assets/icons/add.svg";
+import AddIcon from "./assets/icons/add.svg";
+import Trash from "./assets/icons/trash.svg";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const BACKEND_URL = `${process.env.REACT_APP_BACKEND_URL}`;
+  const form = document.querySelector(".form");
+
+  const addTodo = async (e) => {
+    e.preventDefault();
+    const todo = { newTodo: newTodo };
+    try {
+      await axios.post(`${BACKEND_URL}/todos`, todo);
+      form.reset();
+      setTodos([...todos, { newTodo: newTodo }]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      await axios.delete(`${BACKEND_URL}/todos/${id}`);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const response = await axios.get(`${BACKEND_URL}/todos`);
+        setTodos(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <h1 className="todo__title">to-do list</h1>
-      <div className="todo">
-        <input
-          type="text"
-          name="todoEntry"
-          placeholder="Add a todo"
-        />
-        <button class="todo__btn">
-          <img src={addIcon} />
-        </button>
+    <>
+      <div className="App">
+        <h1 className="todo__title">to-do list</h1>
+        <div className="todo">
+          <form className="form">
+            <input
+              type="text"
+              name="todoEntry"
+              placeholder="Add a todo"
+              onChange={(e) => setNewTodo(e.target.value)}
+            />
+            <button className="todo__btn" onClick={addTodo}>
+              <img src={AddIcon} />
+            </button>
+          </form>
+        </div>
+        {todos?.map((todo) => {
+          return (
+            <div className="todo__container" key={todo.id}>
+              <div>
+                <p>{todo.newTodo}</p>
+              </div>
+              <button
+                className="todo__delete"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                <img className="trash-icon" src={Trash} />
+              </button>
+            </div>
+          );
+        })}
       </div>
-    </div>
+    </>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const PORT = process.env.PORT || 5500;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mysql.createPool({
   host: "localhost",
@@ -12,7 +17,39 @@ const db = mysql.createPool({
   database: "todo_list",
 });
 
-app.get("/", (_req, res) => {});
+app.get("/", (req, res) => {
+  res.send("HELLO!");
+});
+
+app.get("/todos", (req, res) => {
+  const q = "SELECT * FROM todos";
+  db.query(q, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
+  });
+});
+
+app.post("/todos", (req, res) => {
+  const q = "INSERT INTO todos (`newTodo`) VALUES (?)";
+  const values = [req.body.newTodo];
+
+  db.query(q, [values], (err, result) => {
+    if (err) return res.json(err);
+    return res.json("Todo has been added successfully!");
+  });
+});
+
+app,
+  delete ("/:id",
+  (req, res) => {
+    const todoId = req.params.id;
+    const q = "DELETE FROM todos WHERE id = ?";
+
+    db.query(q, todoId, (err, result) => {
+      if (err) return res.json(err);
+      return res.json("Todo was successfully deleted");
+    });
+  });
 
 app.listen(PORT, () => {
   console.log(`The server is running on ${PORT}! You better go catch it`);
